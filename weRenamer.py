702195a8e1cfe8a91eabf91894756427895207e5
Jonathan Harford <jonathan.harford@gmail.com>
2013-06-25 09:09:50 -0400

    Put in actual renaming code.

--- weRenamer.py
+++ weRenamer.py
@@ -2,12 +2,7 @@
 '''
 Created on Jun 8, 2013
 
-@author: Spielford
-
-TODO:
-* Set initial height/width to be 1/3 screen
-* I use ExpandoTextCtrl instead of TextCtrl to keep scrollbars from appearing.
-  Is there a way to do this with TextCtrls? 
+@author: Jonathan Harford  
 
 '''
 
@@ -15,8 +10,6 @@ import logging
 logging.basicConfig(format='%(message)s', level=logging.DEBUG)
 
 import os
-import sys
-import string
 import wx
 from wx.lib.scrolledpanel import ScrolledPanel
 from wx.lib.expando import ExpandoTextCtrl
@@ -26,22 +19,20 @@ class MainWindow(wx.Frame):
     def __init__(self, parent, title):
         
         super(MainWindow,self).__init__(parent, title=title)
-        
         self.init_layout()    
         self.Centre()
         self.Show()
 
     def init_layout(self):
+
         mainpanel = ScrolledPanel(self, -1,
                                   # style = wx.TAB_TRAVERSAL|wx.SUNKEN_BORDER, 
                                   name="panel1")
         mainpanel.SetupScrolling()
         mainsizer = wx.BoxSizer(wx.HORIZONTAL)
         mainpanel.SetSizer(mainsizer)
-        mainpanel.SetAutoLayout(1)
-        mainsizer.Fit(mainpanel)
 
-        self.field1 = ExpandoTextCtrl(mainpanel, style=wx.TE_MULTILINE | wx.TE_READONLY | wx.TE_DONTWRAP)
+        self.field1 = ExpandoTextCtrl(mainpanel, style=wx.TE_MULTILINE | wx.TE_DONTWRAP | wx.TE_READONLY)
         self.field2 = ExpandoTextCtrl(mainpanel, style=wx.TE_MULTILINE | wx.TE_DONTWRAP)
 
         mainsizer.Add(self.field1, 1, wx.GROW)
@@ -54,9 +45,10 @@ class MainWindow(wx.Frame):
         self.mainsizer = mainsizer
 
     def add_filenames(self, filenames):
-        self.field1.AppendText("\n".join(filenames))
-        self.field2.AppendText("\n".join(filenames))
-            
+        self.field1.SetValue("\n".join(filenames))
+        self.field2.SetValue("\n".join(filenames))
+        self.filenames = filenames
+        
     def onKey(self, evt):
         if evt.GetKeyCode() == wx.WXK_ESCAPE:
             self.OnClose(evt)
@@ -64,27 +56,36 @@ class MainWindow(wx.Frame):
             evt.Skip()
 
     def OnClose(self, event):
+        
+        # If there's no changes, we can just close.
+        if self.field1.GetValue() == self.field2.GetValue(): 
+            self.Destroy()
+            return
+
         dlg = wx.MessageDialog(self,
                                "Rename?",
                                "Confirm Renaming", 
                                wx.YES_NO|wx.CANCEL|wx.ICON_QUESTION)
+        
         result = dlg.ShowModal()
         dlg.Destroy()
         if result == wx.ID_YES:
-            logging.info("RENAMING")
             self.Destroy()
-        if result == wx.ID_NO:
+            self.rename(self.field2.GetValue().split("\n"))
+        elif result == wx.ID_NO:
             logging.info("NOT RENAMING")
             self.Destroy()
 
+    def rename(self, new_names):
+        for oldname, newname in zip(self.filenames, new_names):
+            if oldname <> newname:
+                print oldname + " => " + newname
+                os.rename(oldname, newname)
 
 if __name__ == '__main__':
 
-    filenames = os.listdir(".")
-
     app = wx.App(False)
     frame = MainWindow(None, 'weRenamer')
-    frame.add_filenames(filenames)
+    frame.add_filenames(os.listdir("."))
     app.MainLoop()
-    print("done")
 
