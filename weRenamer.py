314856d7c28b04f6b7bd441ff6e3cedfcbf4bb9b
Jonathan Harford <jonathan.harford@gmail.com>
2013-06-28 17:56:00 -0400

    Formatted left panel light grey
    Having trouble with how to format text. Turning text red works in a tiny
    app, but not here. (Nor does it seem to work in the wxPython demo!)

--- weRenamer.py
+++ weRenamer.py
@@ -2,7 +2,7 @@
 '''
 Created on Jun 8, 2013
 
-@author: Jonathan Harford  
+@author: Jonathan Harford
 
 '''
 
@@ -14,58 +14,62 @@ import wx
 from wx.lib.scrolledpanel import ScrolledPanel
 from wx.lib.expando import ExpandoTextCtrl
 
-FONT_FLAGS = (10, 
-              wx.FONTFAMILY_TELETYPE, 
-              wx.FONTSTYLE_NORMAL, 
+FONT_FLAGS = (10,
+              wx.FONTFAMILY_TELETYPE,
+              wx.FONTSTYLE_NORMAL,
               wx.FONTWEIGHT_NORMAL)
 
-
 class MainWindow(wx.Frame):
-    
-    def __init__(self, parent, title):
-        
+
+    def __init__(self, parent, title, filenames, new_names):
         super(MainWindow,self).__init__(parent, title=title)
-        self.init_layout()    
+        self.init_layout()
+        self.set_filenames(filenames, new_names)
         self.Centre()
         self.Show()
 
     def init_layout(self):
 
         mainpanel = ScrolledPanel(self, -1,
-                                  # style = wx.TAB_TRAVERSAL|wx.SUNKEN_BORDER, 
-                                  name="panel1")
+                                  # style = wx.TAB_TRAVERSAL|wx.SUNKEN_BORDER,
+                                  name="mainpanel")
         mainpanel.SetupScrolling()
         mainsizer = wx.BoxSizer(wx.HORIZONTAL)
         mainpanel.SetSizer(mainsizer)
 
-        self.field1 = ExpandoTextCtrl(mainpanel, style=wx.TE_MULTILINE | wx.TE_DONTWRAP | wx.TE_READONLY)
-        self.field1.SetFont(wx.Font(*FONT_FLAGS))
-        self.field2 = ExpandoTextCtrl(mainpanel, style=wx.TE_MULTILINE | wx.TE_DONTWRAP)
-        self.field2.SetFont(wx.Font(*FONT_FLAGS))
+        field1 = ExpandoTextCtrl(mainpanel, style=wx.TE_MULTILINE | wx.TE_READONLY)
+        field1.SetFont(wx.Font(*FONT_FLAGS))
+        field1.SetBackgroundColour('Light Grey')
 
-        mainsizer.Add(self.field1, 1, wx.GROW)
-        mainsizer.Add(self.field2, 1, wx.GROW)
-        
+        field2 = ExpandoTextCtrl(mainpanel, style=wx.TE_MULTILINE)
+        field2.SetFont(wx.Font(*FONT_FLAGS))
+
+        mainsizer.Add(field1, 1, wx.GROW)
+        mainsizer.Add(field2, 1, wx.GROW)
+
         self.Bind(wx.EVT_CLOSE, self.OnClose)
         self.Bind(wx.EVT_CHAR_HOOK, self.onKey)
 
         self.mainpanel = mainpanel
         self.mainsizer = mainsizer
+        self.field1 = field1
+        self.field2 = field2
 
     def set_filenames(self, filenames, new_names):
-            
+
         def strip_textctrl(t):
             t.SetValue(t.GetValue().strip())  # Ugh. More elegant way?
-        
+
+        self.field1.SetDefaultStyle(wx.TextAttr("red"))
         for filename, new_name in zip(filenames,new_names):
             self.field1.AppendText(filename + "\n")
             self.field2.AppendText(new_name + "\n")
         strip_textctrl(self.field1)
         strip_textctrl(self.field2)
-         
+
         self.filenames = filenames
         self.new_names = new_names
-        
+
     def onKey(self, evt):
         if evt.GetKeyCode() == wx.WXK_ESCAPE:
             self.OnClose(evt)
@@ -73,17 +77,17 @@ class MainWindow(wx.Frame):
             evt.Skip()
 
     def OnClose(self, event):
-        
+
         # If there's no changes, we can just close.
-        if self.field1.GetValue() == self.field2.GetValue(): 
+        if not self.field2.IsModified():
             self.Destroy()
             return
 
         dlg = wx.MessageDialog(self,
                                "Rename?",
-                               "Confirm Renaming", 
+                               "Confirm Renaming",
                                wx.YES_NO|wx.CANCEL|wx.ICON_QUESTION)
-        
+
         result = dlg.ShowModal()
         dlg.Destroy()
         if result == wx.ID_YES:
@@ -95,7 +99,7 @@ class MainWindow(wx.Frame):
 
     def update_new_names(self):
         self.new_names = self.field2.GetValue().split("\n")
-        
+
     def rename(self):
         self.update_new_names()
         for oldname, newname in zip(self.filenames, self.new_names):
@@ -104,9 +108,7 @@ class MainWindow(wx.Frame):
                 os.rename(oldname, newname)
 
 if __name__ == '__main__':
-
     app = wx.App(False)
-    frame = MainWindow(None, 'weRenamer')
-    frame.set_filenames(os.listdir("."), os.listdir("."))
+    frame = MainWindow(None, 'weRenamer', os.listdir("."), os.listdir("."))
     app.MainLoop()
 
