867b345a5d73ae0946815de25db1710016741cd2
Jonathan Harford <jonathan.harford@gmail.com>
2013-07-05 11:51:19 -0400

    Added toolbar with toggle buttons.

--- weRenamer.py
+++ weRenamer.py
@@ -14,30 +14,44 @@ import wx
 from wx.lib.scrolledpanel import ScrolledPanel
 from wx.lib.expando import ExpandoTextCtrl
 
-FONT_FLAGS = (10, 
-              wx.FONTFAMILY_TELETYPE, 
-              wx.FONTSTYLE_NORMAL, 
+FONT_FLAGS = (10,
+              wx.FONTFAMILY_TELETYPE,
+              wx.FONTSTYLE_NORMAL,
               wx.FONTWEIGHT_NORMAL)
 
+ICON_SIZE = (16, 16)
 
 class MainWindow(wx.Frame):
     
     def __init__(self, parent, title):
         
-        super(MainWindow,self).__init__(parent, title=title)
+        super(MainWindow, self).__init__(parent, title=title)
         self.init_layout()    
         self.Centre()
         self.Show()
 
     def init_layout(self):
 
-        mainpanel = ScrolledPanel(self, -1,
-                                  # style = wx.TAB_TRAVERSAL|wx.SUNKEN_BORDER, 
-                                  name="panel1")
+        mainpanel = ScrolledPanel(self, -1)
+
         mainpanel.SetupScrolling()
         mainsizer = wx.BoxSizer(wx.HORIZONTAL)
         mainpanel.SetSizer(mainsizer)
 
+        toolbar = self.CreateToolBar(style=wx.TB_3DBUTTONS)
+        
+        ico_showdirs = wx.ArtProvider.GetBitmap(wx.ART_NEW_DIR, wx.ART_TOOLBAR, ICON_SIZE)
+        tool_showdirs = toolbar.AddCheckLabelTool(wx.ID_ANY, "Toggle inclusion of directories", ico_showdirs, shortHelp="Show directories?")
+        # self.Bind(wx.EVT_TOOLBAR, self.showdirs, tool_showdirs)
+        
+        ico_showexts = wx.ArtProvider.GetBitmap(wx.ART_FIND_AND_REPLACE, wx.ART_TOOLBAR, ICON_SIZE)
+        tool_showexts = toolbar.AddCheckLabelTool(wx.ID_ANY, "Toggle inclusion of file extensions", ico_showexts, shortHelp="Show file extensions?")
+        
+        ico_showhids = wx.ArtProvider.GetBitmap(wx.ART_QUESTION, wx.ART_TOOLBAR, ICON_SIZE)
+        tool_showhids = toolbar.AddCheckLabelTool(wx.ID_ANY, "Toggle inclusion of hidden files", ico_showhids, shortHelp="Show hidden files?")
+        
+        toolbar.Realize()
+        
         self.field1 = ExpandoTextCtrl(mainpanel, style=wx.TE_MULTILINE | wx.TE_DONTWRAP | wx.TE_READONLY)
         self.field1.SetFont(wx.Font(*FONT_FLAGS))
         self.field2 = ExpandoTextCtrl(mainpanel, style=wx.TE_MULTILINE | wx.TE_DONTWRAP)
@@ -50,6 +64,7 @@ class MainWindow(wx.Frame):
         self.Bind(wx.EVT_CHAR_HOOK, self.onKey)
 
         self.mainpanel = mainpanel
+        self.toolbar = toolbar
         self.mainsizer = mainsizer
 
     def set_filenames(self, filenames, new_names):
@@ -57,7 +72,7 @@ class MainWindow(wx.Frame):
         def strip_textctrl(t):
             t.SetValue(t.GetValue().strip())  # Ugh. More elegant way?
         
-        for filename, new_name in zip(filenames,new_names):
+        for filename, new_name in zip(filenames, new_names):
             self.field1.AppendText(filename + "\n")
             self.field2.AppendText(new_name + "\n")
         strip_textctrl(self.field1)
@@ -81,8 +96,8 @@ class MainWindow(wx.Frame):
 
         dlg = wx.MessageDialog(self,
                                "Rename?",
-                               "Confirm Renaming", 
-                               wx.YES_NO|wx.CANCEL|wx.ICON_QUESTION)
+                               "Confirm Renaming",
+                               wx.YES_NO | wx.CANCEL | wx.ICON_QUESTION)
         
         result = dlg.ShowModal()
         dlg.Destroy()
