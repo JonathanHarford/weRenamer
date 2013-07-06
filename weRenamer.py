9cc5e31687bf8feb5c40b85ea08d43cdbd1eb11c
Jonathan Harford <jonathan.harford@gmail.com>
2013-07-06 12:54:36 -0400

    Colored left textctrl grey to suggest uneditability.

--- weRenamer.py
+++ weRenamer.py
@@ -2,7 +2,7 @@
 '''
 Created on Jun 8, 2013
 
-@author: Jonathan Harford
+@author: Jonathan Harford  
 
 '''
 
@@ -19,7 +19,6 @@ FONT_FLAGS = (10,
               wx.FONTSTYLE_NORMAL,
               wx.FONTWEIGHT_NORMAL)
 
-<<<<<<< HEAD
 ICON_SIZE = (16, 16)
 
 class RenameCmd(object):
@@ -37,7 +36,7 @@ class RenameCmd(object):
         
     
     def __str__(self):
-        return self.fulloldname + " => " + self.fullnewname + " (" + self.filetype + ")"
+        return '''{}: "{}" => "{}"'''.format(self.filetype, self.fulloldname, self.fullnewname)
     
     @property
     def fulloldname(self):
@@ -67,14 +66,6 @@ class MainWindow(wx.Frame):
         
         super(MainWindow, self).__init__(parent, title=title)
         self.init_layout()    
-=======
-class MainWindow(wx.Frame):
-
-    def __init__(self, parent, title, filenames, new_names):
-        super(MainWindow,self).__init__(parent, title=title)
-        self.init_layout()
-        self.set_filenames(filenames, new_names)
->>>>>>> 314856d7c28b04f6b7bd441ff6e3cedfcbf4bb9b
         self.Centre()
         self.Show()
 
@@ -86,19 +77,12 @@ class MainWindow(wx.Frame):
         
     def init_layout(self):
 
-<<<<<<< HEAD
         mainpanel = ScrolledPanel(self, -1)
 
-=======
-        mainpanel = ScrolledPanel(self, -1,
-                                  # style = wx.TAB_TRAVERSAL|wx.SUNKEN_BORDER,
-                                  name="mainpanel")
->>>>>>> 314856d7c28b04f6b7bd441ff6e3cedfcbf4bb9b
         mainpanel.SetupScrolling()
         mainsizer = wx.BoxSizer(wx.HORIZONTAL)
         mainpanel.SetSizer(mainsizer)
 
-<<<<<<< HEAD
         toolbar = self.CreateToolBar(style=wx.TB_3DBUTTONS)
         
         ico_showdirs = wx.ArtProvider.GetBitmap(wx.ART_NEW_DIR, wx.ART_TOOLBAR, ICON_SIZE)
@@ -113,22 +97,16 @@ class MainWindow(wx.Frame):
         
         toolbar.Realize()
         
-        self.field1 = ExpandoTextCtrl(mainpanel, style=wx.TE_MULTILINE | wx.TE_DONTWRAP | wx.TE_READONLY)
-        self.field1.SetFont(wx.Font(*FONT_FLAGS))
-        self.field2 = ExpandoTextCtrl(mainpanel, style=wx.TE_MULTILINE | wx.TE_DONTWRAP)
-        self.field2.SetFont(wx.Font(*FONT_FLAGS))
-=======
         field1 = ExpandoTextCtrl(mainpanel, style=wx.TE_MULTILINE | wx.TE_READONLY)
         field1.SetFont(wx.Font(*FONT_FLAGS))
-        field1.SetBackgroundColour('Light Grey')
-
+        field1.SetBackgroundColour(wx.LIGHT_GREY)
+        
         field2 = ExpandoTextCtrl(mainpanel, style=wx.TE_MULTILINE)
         field2.SetFont(wx.Font(*FONT_FLAGS))
 
         mainsizer.Add(field1, 1, wx.GROW)
         mainsizer.Add(field2, 1, wx.GROW)
->>>>>>> 314856d7c28b04f6b7bd441ff6e3cedfcbf4bb9b
-
+        
         self.Bind(wx.EVT_CLOSE, self.OnClose)
         self.Bind(wx.EVT_CHAR_HOOK, self.onKey)
 
@@ -138,7 +116,6 @@ class MainWindow(wx.Frame):
         self.field1 = field1
         self.field2 = field2
 
-<<<<<<< HEAD
     def init_renames(self, filenames):
         self.rename_cmds = [RenameCmd(filename) for filename in filenames]
 
@@ -157,22 +134,6 @@ class MainWindow(wx.Frame):
         newnames = self.field2.GetValue().split("\n")
         for rename, newname in zip(self.rename_cmds, newnames):
             rename.refresh(newname)
-=======
-    def set_filenames(self, filenames, new_names):
-
-        def strip_textctrl(t):
-            t.SetValue(t.GetValue().strip())  # Ugh. More elegant way?
-
-        self.field1.SetDefaultStyle(wx.TextAttr("red"))
-        for filename, new_name in zip(filenames,new_names):
-            self.field1.AppendText(filename + "\n")
-            self.field2.AppendText(new_name + "\n")
-        strip_textctrl(self.field1)
-        strip_textctrl(self.field2)
-
-        self.filenames = filenames
-        self.new_names = new_names
->>>>>>> 314856d7c28b04f6b7bd441ff6e3cedfcbf4bb9b
 
     def onKey(self, evt):
         if evt.GetKeyCode() == wx.WXK_ESCAPE:
@@ -181,71 +142,39 @@ class MainWindow(wx.Frame):
             evt.Skip()
 
     def OnClose(self, event):
-<<<<<<< HEAD
         
         ## If there's no changes, we can just close.
         
         if self.field1.GetValue() == self.field2.GetValue(): 
-=======
-
-        # If there's no changes, we can just close.
-        if not self.field2.IsModified():
->>>>>>> 314856d7c28b04f6b7bd441ff6e3cedfcbf4bb9b
             self.Destroy()
             logging.info("Renaming canceled.")
             return
 
         dlg = wx.MessageDialog(self,
-<<<<<<< HEAD
                                "RenameCmd?",
                                "Confirm Renaming",
                                wx.YES_NO | wx.CANCEL | wx.ICON_QUESTION)
         
-=======
-                               "Rename?",
-                               "Confirm Renaming",
-                               wx.YES_NO|wx.CANCEL|wx.ICON_QUESTION)
-
->>>>>>> 314856d7c28b04f6b7bd441ff6e3cedfcbf4bb9b
         result = dlg.ShowModal()
         
         dlg.Destroy()
 
         if result == wx.ID_YES:
-            self.rename()
             self.Destroy()
-<<<<<<< HEAD
             self.refresh_renames()
             for r in self.rename_cmds:
                 r.execute()
                 
-=======
->>>>>>> 314856d7c28b04f6b7bd441ff6e3cedfcbf4bb9b
         elif result == wx.ID_NO:
             logging.info("Renaming canceled.")
             self.Destroy()
 
-<<<<<<< HEAD
 
 if __name__ == '__main__':
     app = wx.App(False)
     frame = MainWindow(None, 'weRenamer')
     frame.init_renames(os.listdir("."))
     frame.load_renames_into_textctrls()
-=======
-    def update_new_names(self):
-        self.new_names = self.field2.GetValue().split("\n")
-
-    def rename(self):
-        self.update_new_names()
-        for oldname, newname in zip(self.filenames, self.new_names):
-            if oldname <> newname:
-                print oldname + " => " + newname
-                os.rename(oldname, newname)
-
-if __name__ == '__main__':
-    app = wx.App(False)
-    frame = MainWindow(None, 'weRenamer', os.listdir("."), os.listdir("."))
->>>>>>> 314856d7c28b04f6b7bd441ff6e3cedfcbf4bb9b
     app.MainLoop()
+
 
